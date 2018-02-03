87dd6866679a20b58576e7c78037b734efa95a18
Daniel Shih <danielshihtiger@gmail.com>
2018-02-03 15:00:21 -0800

    Deploy attempt..

---
+++ simple_server/index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+   console.log('one user connected: ' + socket.id);
+   
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on port %d', port);
+});
+    

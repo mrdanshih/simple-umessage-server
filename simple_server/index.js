7840cf8b0f54b753d482b326fc8fbab060d6ab1c
Daniel Shih <danielshihtiger@gmail.com>
2018-02-03 21:46:02 -0800

    Store phone numbers in server now

--- simple_server/index.js
+++ simple_server/index.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var clients = {};
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
@@ -10,19 +12,32 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
    console.log('one user connected: ' + socket.id);
    
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message from ', msg + " " + socket.id);
+   socket.on('chat message', function(msg){
+     io.emit('chat message', msg);
+     console.log('message from ', msg + " " + socket.id);
+   });
 
-    socket.broadcast.to(socket.id).emit('chat message', 'Hello there, ' 
-    	+ socket.id + '. You sent me ' + socket.id + ' !!!!') 
+   socket.on('handshake phone', function(num){
+   	clients[num] = socket;
+   });
 
-  });
-});
+ });
+
 
 http.listen(port, function(){
   console.log('listening on port %d', port);
 });
 
+
+function doSomeLogging() {
+	console.log(Object.keys(clients));
+
+	for(num in clients) {
+     	 clients[num].emit('chat message', 'Hello there, I have your number: ' 
+    	+ num);
+     }
+}
+
+var interval = setInterval(doSomeLogging, 2000);
 
     
